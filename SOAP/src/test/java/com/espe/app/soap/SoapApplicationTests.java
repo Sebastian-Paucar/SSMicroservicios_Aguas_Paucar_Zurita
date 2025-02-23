1fa68a42648a0af0b88771cfe0efe10671f19a28
Sebastian-Paucar <jspaucar1@gmail.com>
2025-02-22 23:40:59 -0500

    Changes

--- SOAP/src/test/java/com/espe/app/soap/SoapApplicationTests.java
+++ SOAP/src/test/java/com/espe/app/soap/SoapApplicationTests.java
@@ -1,10 +1,7 @@
 package com.espe.app.soap;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SoapApplicationTests {
-
 
 }
