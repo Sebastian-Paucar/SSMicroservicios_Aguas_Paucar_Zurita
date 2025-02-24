e6e0c509e00d4fcc1db4d8db78498e9b8868bfe8
Sebastian-Paucar <jspaucar1@gmail.com>
2025-02-24 02:28:49 -0500

    env changes

--- security/src/main/java/com/espe/app/security/client/UsuarioClient.java
+++ security/src/main/java/com/espe/app/security/client/UsuarioClient.java
@@ -4,13 +4,17 @@ package com.espe.app.security.client;
 import com.espe.app.security.config.FeignClientConfig;
 import com.espe.app.security.entity.Usuario;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "msvc-usuarios", url = "http://localhost:2000", configuration = FeignClientConfig.class)
+
+
+@FeignClient(name = "msvc-usuarios", url = "${client.usuarios.uri}", configuration = FeignClientConfig.class)
 public interface UsuarioClient {
+
     @GetMapping("/crud/usuarios/email/{email}")
     Usuario buscarPorEmail(@PathVariable("email") String name);
 }
